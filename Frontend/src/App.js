7c39ed8b7b80c9513fba8d76011e637cc16e6c7f
Than Thi Thao <thanthao051102@gmail.com>
2023-11-20 10:42:15 +0700

    add lazyload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,16 @@
-import { Routes, Route ,BrowserRouter } from 'react-router-dom'
-import LoginPage from './views/LoginPage/LoginPage';
+import React, { lazy, Suspense } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+const LazyLoginPage = lazy(() => import('./views/LoginPage/LoginPage'));
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LoginPage/>} />
+        <Route path="/" element={<Suspense fallback={<div>Loading...</div>}><LazyLoginPage /></Suspense>} />
       </Routes>
     </BrowserRouter>
   );
 }
-export default App;
 
+export default App;
