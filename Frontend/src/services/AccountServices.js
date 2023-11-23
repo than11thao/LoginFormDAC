6e15086e1205105be57b2f4393d197e39b9a1370
Than Thi Thao <thanthao051102@gmail.com>
2023-11-23 14:33:02 +0700

    update service

---
+++ Frontend/src/services/AccountServices.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
+
+class AccountServices {
+    //Call request API login by method POST
+    getAllAccount(token) {
+        return axios.get('/all_user', {
+            headers: {
+                'Authorization': `Basic ${token}`
+            }
+        });
+    }
+
+    postNewAccount(data, token) {
+        return axios.post('/new_user', data, {
+            headers: {
+                'Authorization': `Basic ${token}`
+            }
+        });
+    }
+
+    searchAccount(data,token) {
+        return axios.post('/user_info', data, {
+            headers: {
+                'Authorization': `Basic ${token}`
+            }
+        });
+    }
+
+    updateAccount(data, token) {
+        return axios.put('/update_user', data, {
+            headers: {
+                'Authorization': `Basic ${token}`
+            }
+        });
+    }
+}
+
+export default new AccountServices();
+
